c7dc462d6126e93c6742a87461a5f6f487ed06dc
mostafa.salah <mostafa.salah@tpay.me>
2025-01-21 00:18:14 +0200

    fixing leaderboard api issue

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,6 +1,29 @@
 import { createClient } from "@/utils/supabase/server";
 import { type NextRequest, NextResponse } from "next/server";
 
+interface Profile {
+  id: string;
+  first_name: string;
+  last_name: string;
+}
+
+interface Competition {
+  id: string;
+  name: string;
+  start_date: string;
+  end_date: string;
+  prize_amount: number;
+}
+
+interface UserCompetition {
+  user_id: string;
+  id: string;
+  competition_id: string;
+  points_earned: number;
+  profiles: Profile;
+  competitions: Competition;
+}
+
 export async function GET(req: NextRequest) {
   const supabase = await createClient();
 
@@ -29,7 +52,8 @@ export async function GET(req: NextRequest) {
     .gt("competitions.end_date", new Date().toISOString())
     .lt("competitions.start_date", new Date().toISOString())
     .order("points_earned", { ascending: false })
-    .limit(10);
+    .limit(10)
+    .returns<UserCompetition[]>();
 
   if (error) {
     console.error("Error fetching choices:", error);
@@ -38,7 +62,7 @@ export async function GET(req: NextRequest) {
   if (!user_competitions) {
     return NextResponse.json([]);
   }
-  const leaderBoard = user_competitions.map((item) => ({
+  const leaderBoard = user_competitions.map((item: UserCompetition) => ({
     id: item.user_id,
     name: item.profiles.first_name,
     score: item.points_earned,
